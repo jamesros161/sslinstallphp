dde8c12583d8f605f790675fcf24b1ac881b34fb
James Rosado <dev@twmsllc.com>
2019-04-26 05:47:39 -0400

    test

--- domain.php
+++ domain.php
@@ -79,16 +79,16 @@ class Dom
              
         $result = curl_exec($chdcv);
         curl_close($chdcv);
-        if (strpos($result, '301 Moved') == true){
+        if (strpos($result, '301 Moved') !== false){
             echo '\n DCV File has a Redirect\n';
-        } elseif(strpos($result, '404') == true){
+        } elseif(strpos($result, '404') !== false){
             echo '\n DCV File Not Found';
-        } elseif(strpos($result, '403') == true){
+        } elseif(strpos($result, '403') !== false){
             echo '\n DCV File Permission Denied';
-        } elseif(strpos($result, '200') == true){
+        } elseif(strpos($result, '200') !== false){
             echo '\n DCV File Exists w/o Redirect\n';
         }
-        
+
         //return explode("\n", $result);
         return $result;
     }
