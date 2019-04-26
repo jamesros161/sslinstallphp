43b78b9a81539f8bbac936fdebfa1ddf1d574274
James Rosado <dev@twmsllc.com>
2019-04-25 20:24:43 -0400

    test

--- domain.php
+++ domain.php
@@ -1,48 +1,50 @@
 <?php
 require "whm.php";
+class CsrInputData 
+{
+    public function __construct() {
+        $this->domainName   = "";
+        $this->emailAdd     = "";
+        $this->locality     = "";
+        $this->state        = "";
+        $this->country      = "";
+        $this->org          = "";
+        $this->unit         = "";
+    }
+}
 
 class Dom
 {
     public function __construct() {
         $this->whm1 = new WHM;
-        $this->csrData = "TEST CSR DAT VAR";
-        $this->sslCert = "TEST SSL CERT VAR";
-
-        $this->csrInputData = array(
-            "domainName"=> "",
-            "emailAdd"=> "",
-            "locality"=> "",
-            "state"=> "",
-            "country"=> "",
-            "org"=> "",
-            "unit"=> ""
-        );
+        $this->csrInputData = new CsrInputData;
     }
+
     // loop through each element in the $argv array
     public function getInputVars() {
 
         $val = getopt("d:e::l::s::c::o::u::tT");
        // var_dump($val);
         if (array_key_exists("d", $val)) {
-            $this->csrInputData["domainName"] = $val["d"];
+            $this->csrInputData->domainName = $val["d"];
         }
         if (array_key_exists("e", $val)) {
-            $this->csrInputData["emailAdd"] = $val["e"];
+            $this->csrInputData->emailAdd = $val["e"];
         }
         if (array_key_exists("l", $val)) {
-            $this->csrInputData["locality"] = $val["l"];
+            $this->csrInputData->locality = $val["l"];
         }
         if (array_key_exists("s", $val)) {
-            $this->csrInputData["state"] = $val["s"];
+            $this->csrInputData->state = $val["s"];
         }
         if (array_key_exists("c", $val)) {
-            $this->csrInputData["country"] = $val["c"];
+            $this->csrInputData->country = $val["c"];
         }
         if (array_key_exists("o", $val)) {
-            $this->csrInputData["org"] = $val["o"];
+            $this->csrInputData->org = $val["o"];
         }
         if (array_key_exists("u", $val)) {
-            $this->csrInputData["unit"] = $val["u"];
+            $this->csrInputData->unit = $val["u"];
         }
         echo "csrInputData: \n";
         //var_dump($this->csrInputData);
@@ -50,7 +52,7 @@ class Dom
 
     public function getDomainData() {
         //echo $this->csrInputData["domainName"];
-        $domainData = $this->whm1->getDomainData($this->csrInputData["domainName"]);
+        $domainData = $this->whm1->getDomainData($this->csrInputData->domainName);
         //var_dump($domainData);
         print_r($domainData);
     }
