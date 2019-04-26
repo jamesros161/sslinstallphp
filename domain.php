7931a976d440de86797e438f72d8406a0586ec46
James Rosado <dev@twmsllc.com>
2019-04-26 00:30:02 -0400

    test

--- domain.php
+++ domain.php
@@ -24,7 +24,7 @@ class Dom
         $this->getInputVars();
 
         if ($this->testing->testMode == true){
-            $filename = "/testCsrinputData.json";
+            $filename = "/testCsrInputData.json";
             $this->csrInputData           = json_decode(file_get_contents($this->testing->path . $filename));
             print_r($this->csrInputData);
         } else{
