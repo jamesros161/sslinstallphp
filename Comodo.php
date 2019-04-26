61fa4fb79ea97292da6e7154e539c45871fcd6cf
James Rosado <dev@twmsllc.com>
2019-04-26 07:53:12 -0400

    test

--- Comodo.php
+++ Comodo.php
@@ -88,7 +88,7 @@ class Comodo
         print_r($argsArray);    
         $argsQuery = http_build_query($argsArray);
         $callResult = $this->call([$this->urls->autoApplySsl, $argsQuery, count($argsArray)]);
-        print_r(json_decode($callResult));
+        print_r($callResult);
         return $callResult;
     }
 
