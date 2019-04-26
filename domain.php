e052430dc71dc424206e502ceec5d181851d9c6c
James Rosado <dev@twmsllc.com>
2019-04-25 21:31:49 -0400

    test

--- domain.php
+++ domain.php
@@ -8,13 +8,13 @@ class Dom
         $this->whm1 = new WHM;
         //$this->com = new Comodo;
 
-        $this->csrInputData->domainName   = " ";
-        $this->csrInputData->emailAdd     = " ";
-        $this->csrInputData->locality     = " ";
-        $this->csrInputData->state        = " ";
-        $this->csrInputData->country      = " ";
-        $this->csrInputData->org          = " ";
-        $this->csrInputData->unit         = " ";   
+        $this->csrInputData->domainName   = false;
+        $this->csrInputData->emailAdd     = false;
+        $this->csrInputData->locality     = false;
+        $this->csrInputData->state        = false;
+        $this->csrInputData->country      = false;
+        $this->csrInputData->org          = false;
+        $this->csrInputData->unit         = false;   
 
         $this->csrData                    = $this->whm1->getCsrData($this->csrInputData);
         $this->domainData                 = $this->whm1->getDomainData($this->csrInputData->domainName);
@@ -31,25 +31,46 @@ class Dom
        // var_dump($val);
         if (array_key_exists("d", $val)) {
             $this->csrInputData->domainName = $val["d"];
+        } else {
+            $this->csrInputData->domainName = "";
         }
+
         if (array_key_exists("e", $val)) {
             $this->csrInputData->emailAdd = $val["e"];
+        } else {
+            $this->csrInputData->emailAdd = "";
         }
+
         if (array_key_exists("l", $val)) {
             $this->csrInputData->locality = $val["l"];
+        } else {
+            $this->csrInputData->locality = "";
         }
+
         if (array_key_exists("s", $val)) {
             $this->csrInputData->state = $val["s"];
+        } else {
+            $this->csrInputData->state = "";
         }
+
         if (array_key_exists("c", $val)) {
             $this->csrInputData->country = $val["c"];
+        } else {
+            $this->csrInputData->country = "";
         }
+
         if (array_key_exists("o", $val)) {
             $this->csrInputData->org = $val["o"];
+        } else {
+            $this->csrInputData->org = "";
         }
+
         if (array_key_exists("u", $val)) {
             $this->csrInputData->unit = $val["u"];
+        } else {
+            $this->csrInputData->unit = "";
         }
+
         echo "csrInputData: \n";
         //var_dump($this->csrInputData);
     }
