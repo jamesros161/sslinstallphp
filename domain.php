3e5ecf0f33c6db2604cc7e7e3d626e3d1e661be1
James Rosado <dev@twmsllc.com>
2019-04-26 22:34:38 -0400

    test

--- domain.php
+++ domain.php
@@ -99,8 +99,8 @@ class Dom
         }
 
         $this->whm1->sslInstall($this->csrInputData->domainName, $this->csrData->data->key, $this->certificate);
-        $fp = json_encode($this->com->getFingerPrint($this->certificate->cert));
-        print_r(str_replace(":", "", $fp)); 
+        $fp = explode(":", $this->com->getFingerPrint($this->certificate->cert));
+        print_r($fp); 
         $this->com->sslChecker($this->csrInputData->domainName, $this->csrHashes->sha256);
     }
 
