8bd9634682d6a40bb03a7bdf17d376276e72ebbd
James Rosado <dev@twmsllc.com>
2019-04-26 22:44:55 -0400

    test

--- Comodo.php
+++ Comodo.php
@@ -149,7 +149,7 @@ class Comodo
             "url"             =>  $domain);
 
         //print_r($argsArray);
-        sleep(30);
+        sleep(10);
         $argsQuery = http_build_query($argsArray);
         $callResult = $this->call([$this->urls->sslChecker, $argsQuery, count($argsArray)]);
         parse_str($callResult[0], $output);
