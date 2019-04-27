6d239fb69404105404c239ee48fc0cad6afc9d5c
James Rosado <dev@twmsllc.com>
2019-04-26 21:26:16 -0400

    test

--- Comodo.php
+++ Comodo.php
@@ -152,7 +152,11 @@ class Comodo
         $callResult = $this->call([$this->urls->sslChecker, $argsQuery, count($argsArray)]);
         parse_str($callResult[0], $output);
         print_r($output);
-        if($output['cert_fingerprint_sha256'] == $sha256)
+        if($output['cert_fingerprint_sha256'] == $sha256) {
+            echo "\nSSL Installation Verified by SSL Checker\n";
+        } else {
+            die("SSL Installation Failure\n\tInstalled Certificate does not Match purchased Certificate\n");
+        }
         return $output;
     }
 
